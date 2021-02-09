55d45d82b97b3c6ff55b94f25b5400404d4f6760
JuanMartinCoder <rodriguezjuanmartin66@gmail.com>
2021-02-09 17:02:49 -0300

    make better consulting dates and handling errors and exceptions, and started modal feature

--- app/components/screens/home/HomeScreen.js
+++ app/components/screens/home/HomeScreen.js
@@ -1,8 +1,9 @@
 import React , { useEffect, useState } from 'react'
-import { Text, View,StyleSheet,ScrollView ,TouchableOpacity} from 'react-native'
+import { Text, View,StyleSheet,ScrollView ,TouchableOpacity,Modal, LogBox} from 'react-native'
 // import Calendar from './Calendar'
 import CalendarStrip from 'react-native-calendar-strip';
 import Exercises from './Exercises';
+
 
 var today = new Date();
 var Changedtoday = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0') + '-' + String(today.getDate()).padStart(2, '0');;
@@ -12,13 +13,14 @@ export default function HomeScreen() {
 
         const [isLoading, setLoading] = useState(true);
         const [data, setData] = useState([]);
+        const [isvisible, setVisible] = useState(false);
         
         var existData = false;
+        var url = 'http://192.168.0.110:4000/setsbydate/'+ Changedtoday;
 
         useEffect(() => {
-            var url = 'http://192.168.0.110:4000/setsbydate/2021-01-31'; // + Changedtoday
-            console.log(url);
-            fetch(url)
+          
+              fetch(url)
               .then((response) => response.json())
               .then((json) => setData(json))
               .catch((error) => console.error(error))
@@ -40,8 +42,8 @@ export default function HomeScreen() {
                <View style={styles.body}>
                 <CalendarStrip
                     scrollable
-                    startingDate={today}
-                    selectedDate={today}
+                    startingDate={Changedtoday}
+                    selectedDate={Changedtoday}
                     style={{height:150, paddingTop: 20, paddingBottom: 0,
                       shadowColor: 'black',
                       shadowOpacity: 0.5,
@@ -55,7 +57,16 @@ export default function HomeScreen() {
                     highlightDateNumberStyle={{color: '#6FE6D6'}}
                     highlightDateNameStyle={{color: '#6FE6D6'}}
                     onDateSelected={(date) => {
-                        console.log(date.format('YYYY-MM-DD').toString());
+                      url = 'http://192.168.0.110:4000/setsbydate/'+ date.format('YYYY-MM-DD').toString();
+                      
+                      fetch(url)
+                      .then((response) => response.json())
+                      .then((json) => setData(json))
+                      .catch((error) => console.error(error))
+                      .finally(() => {setLoading(false);});
+                      
+                      
+
                 } }   //date = YYYY-MM-DD         
                     
                 
@@ -66,9 +77,36 @@ export default function HomeScreen() {
                   {
                     existData ? <Exercises props={data}/> : <Text style={styles.errorMessage}>No existe una rutina este dia. Aprete en el + para agregar una rutina.</Text>
                   }
-                  
+                  {/*        MODAL      */}
+                  <Modal
+                    animationType="fade"
+                    transparent={true}
+                    visible={isvisible}
+                    onRequestClose={() => {
+                      setVisible(!isvisible);
+                    }}> 
+
+                      <View style={styles.centeredView}>
+                          <View style={styles.modalView}>
+
+
+
+
+                            <TouchableOpacity style={styles.buttonModal, styles.button} onPress={()=>setVisible(!isvisible)}>
+
+                            </TouchableOpacity>  
+                          </View>
+                       </View>
+
+                    
+                  </Modal> 
+
+
+
+
+                  {    /*BUTTON*/        }
                   <View style={styles.buttonContainer}>
-                  <TouchableOpacity style={styles.button}>
+                  <TouchableOpacity style={styles.button} onPress={()=>setVisible(true)}>
                       <Text style={styles.buttonText}>+</Text>
                   </TouchableOpacity>
                   </View>
@@ -97,6 +135,32 @@ const styles = StyleSheet.create({
         flex:1,
         color: '#A1A1A1',
         padding: 30
+      },
+      buttonModal:{
+          alignSelf:'center'
+      },
+      centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor:'#00000020'
+      },    
+      modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        height:100,
+        width:100,
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
       },
       button:{
         backgroundColor: "#6FE6D6",
